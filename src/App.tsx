f851d57d40f83f93284d93912af6d50ec138c605
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-02 14:01:32 -0500

    add sonner library and integrate Toaster component in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import { Toaster } from 'sonner';
 import { CreateNewUser } from './components/CreateNewUser';
 import { ListOfUsers } from './components/ListOfUsers';
 
 const App = () => {
   return (
     <div className="max-w-7xl flex flex-col gap-10 mx-auto p-4">
+      <Toaster />
       <ListOfUsers />
       <CreateNewUser />
     </div>
