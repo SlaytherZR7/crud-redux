21f0d623dfb71f5d2878b095b5c441498b406eb3
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-02 13:06:22 -0500

    add middleware for persisting Redux state to local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,18 @@
-import { configureStore } from '@reduxjs/toolkit';
+import { type Middleware, configureStore } from '@reduxjs/toolkit';
 import usersReducer from './users/slice';
+
+const persistanceLocalStorageMiddleware: Middleware =
+  (store) => (next) => (action) => {
+    next(action);
+    localStorage.setItem('redux_state', JSON.stringify(store.getState()));
+  };
 
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(persistanceLocalStorageMiddleware);
   },
 });
 
