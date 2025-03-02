96827a98bdc4d9ac0b2dff314c6fc5d081fe8c7a
SlaytherZR7 <slayther.zamora@epn.edu.ec>
2025-03-02 11:56:05 -0500

    add Redux store setup and integrate with App component

--- src/index.tsx
+++ src/index.tsx
@@ -3,12 +3,17 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { Provider } from 'react-redux';
+import { store } from './store';
+
 const rootEl = document.getElementById('root');
 if (rootEl) {
   const root = ReactDOM.createRoot(rootEl);
   root.render(
     <React.StrictMode>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </React.StrictMode>,
   );
 }
